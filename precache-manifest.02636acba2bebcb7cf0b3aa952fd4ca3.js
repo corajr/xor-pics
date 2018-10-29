50fc1b921a9e7e96537bb85a25484e69964ec943
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2018-10-28 22:38:15 -0400

    Updates

---
+++ precache-manifest.02636acba2bebcb7cf0b3aa952fd4ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2342b05e19639d46eb7fb727b3ce8dd",
+    "url": "/static/media/logo.e2342b05.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca022ea573b1eb0e2f41",
+    "url": "/static/js/main.ca022ea5.chunk.js"
+  },
+  {
+    "revision": "6f333d8b1170895ebe1a",
+    "url": "/static/js/1.6f333d8b.chunk.js"
+  },
+  {
+    "revision": "ca022ea573b1eb0e2f41",
+    "url": "/static/css/main.34aa3698.chunk.css"
+  },
+  {
+    "revision": "875f15df23f351b6470fab43ed16d411",
+    "url": "/index.html"
+  }
+];
